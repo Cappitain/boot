b209d81fa45ed646ce947f97c5e52ec78906b184
Cappitain <161675148+Cappitain@users.noreply.github.com>
2024-10-18 03:15:49 +0700

    New

--- boots/src/app/app.component.ts
+++ boots/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet], // ตรวจสอบให้แน่ใจว่า RouterOutlet อยู่ใน array imports
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'boots';
+
+  constructor(private router: Router) {}
+
+  goToLogin() {
+    this.router.navigate(['/page/login.component.html']);
+  }
 }
--- boots/src/app/app.routes.ts
+++ boots/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './page/login/login.component'; // ใส่ path ที่ถูกต้องสำหรับหน้า login ของคุณ
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // เปลี่ยนเป็นหน้า default ของคุณ
   { path: 'login', component: LoginComponent },
   // เพิ่มหน้าอื่นๆ ที่ต้องการ
