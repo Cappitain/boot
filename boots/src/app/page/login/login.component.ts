85e4e1b11d98189d8abbdf96ebb0eb2964c3de5d
Cappitain <161675148+Cappitain@users.noreply.github.com>
2024-10-18 15:54:40 +0700

    back to main

--- boots/src/app/page/login/login.component.ts
+++ boots/src/app/page/login/login.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent {    
+  constructor(private router: Router) {}
 
+  goTomain() {
+    this.router.navigate(['']);
+  }
 }
