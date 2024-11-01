067881f5862e72b43c9cd70aa815fb796170bb0f
Panupat1001 <panupat.p@ku.th>
2024-11-01 23:36:28 +0700

    event

--- src/app/page/event/event.component.ts
+++ src/app/page/event/event.component.ts
@@ -1,12 +1,71 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
+import { DataService } from '../../service/data.service';
+import { HttpClient } from '@angular/common/http';
+import { Convert as zoneCvt, Zone } from '../../model/zone.model';  
+import { Convert as boothCvt, Booth } from '../../model/booth.model';
+import { MatListModule, MatListOption } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-event',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet,MatListModule,CommonModule],
   templateUrl: './event.component.html',
   styleUrl: './event.component.scss'
 })
 export class EventComponent {
+  zones = Array<Zone>(); 
+  booths = Array<Booth>();
+  events = Array<Event>();
+  selectedZone: Zone | undefined;
+  filteredBooths: Booth[] = []; 
 
+  constructor(private route: ActivatedRoute,private router: Router,private dataService:DataService, private http:HttpClient){
+    http.get(dataService.apiEndpoint + "/zone").subscribe((data:any)=>{
+      this.zones = zoneCvt.toZone(JSON.stringify(data));
+      console.log(this.zones);
+    });
+
+    http.get(dataService.apiEndpoint + "/booth").subscribe((data:any)=>{
+      this.booths = boothCvt.toBooth(JSON.stringify(data));
+      console.log(this.booths);
+    });
+
+
+
+
+
+  }
+  countBoothsInZone(zone: Zone): number {
+    return this.booths.filter(booth => booth.zoneID === zone.zoneID).length;
+  }
+
+  show(option: MatListOption) {
+    this.selectedZone = option.value;
+    // กรองบูธที่อยู่ในโซนที่เลือก
+    this.filteredBooths = this.booths.filter(booth => booth.zoneID === this.selectedZone?.zoneID);
+    console.log(this.selectedZone, this.filteredBooths);
+  }
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
+  goToRegister() {
+    this.router.navigate(['/regis']);
+  }
+  goToZone() {
+    this.router.navigate(['/zone']);
+  }
+  goTomain() {
+    this.router.navigate(['']);
+  }
+  goTomain2() {
+    this.router.navigate(['/main2']);
+  }
 }
+
+
+
--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class DataService {
+  event: any;
   zones: any;
   selectedzones: any;
   booths: any;
